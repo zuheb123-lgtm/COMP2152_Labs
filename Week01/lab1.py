45d19d21211be89adece562b4bfabc497dd807cc
fl33zyyy.7 <kanalabdi7@gmail.com>
2026-01-16 09:56:47 -0500

    add lab 01

---
+++ Week01/lab1.py
@@ -0,0 +1,24 @@
+# Sample Coding Questions 01 Week 01
+# Zuheb Mohamud
+
+# Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracketed Version of: e = a - b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+print("Question 3 result:", e)
+
+# Question 4: Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Question 5: User Input
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age:", userAge)
